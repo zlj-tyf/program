874f4fc324b7ad56d667f99997039332fb7339fa
zlj-tyf <zljzljsweepy@qq.com>
2025-07-31 12:46:20 +0800

    For database.php: Updated the database; For addLog.pjp: Added the function to locate current date and autofill in logdate parameter

--- config/database.php
+++ config/database.php
@@ -1,4 +1,4 @@
 <?php
 //$db = @mysqli_connect("数据库地址","用户名","密码","数据库名")
-$db = @mysqli_connect("localhost","hw","hwhwhw","hw")
+$db = @mysqli_connect("localhost","root","123456","school")
 or die("Fail to connect to Server");
--- user/addLog.php
+++ user/addLog.php
@@ -5,14 +5,23 @@
     <link rel="stylesheet" type="text/css" href="css/fun.css">
 </head>
 <body>
-    <h3>奖惩录入</h3>
+    <h3>信息更新</h3>
 <form style="margin:30px;"action="./addLogFunc.php" method="post" id="log">
 
-    <span>类型：</span><input name="type" required type="radio" value="1" >奖<input name="type" type="radio" value="0">惩<br>
-    <span>时间：</span><input name="logdate" required type="date" style="width:180px"><br>
+    <span>类型：</span><input name="type" required type="radio" value="1" >更新项目<input name="type" type="radio" value="0">新建项目<br>
+    <!-- <span>时间：</span><input name="logdate" required type="date" style="width:180px"><br> -->
+    <span>时间：</span>
+<input name="logdate" required type="date" style="width:180px" id="logdate"><br>
+
+<script>
+// 获取当前日期（格式：YYYY-MM-DD）
+const today = new Date().toISOString().split('T')[0];
+// 设置默认值为今天
+document.getElementById('logdate').value = today;
+</script>
     <span>缘由：</span><input name="reason"required  type="text" class="boxwidth">
     <br>
-    <span>详情：</span><br><textarea style="display:block;width:90%;height:60px;"name="detail" required form="log"></textarea><br>
+    <span>详情：<br/>请引用前端页面链接</span><br><textarea style="display:block;width:90%;height:60px;"name="detail" required form="log"></textarea><br>
     <span></span><input name="submit" type="submit" value="提交"><br>
 </form>
 </body>
